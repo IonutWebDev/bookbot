379d45bfc74c64595f169529661d1c64477b9346
IonutWebDev <vinteionut@yahoo.com>
2024-03-18 17:55:53 +0200

    Added tools.py to better organize code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+from tools import *
+
 def main():
     print("--- Begin report of books/frankenstein.txt ---")
 
@@ -18,30 +20,5 @@ def main():
     # Print the total occurences of each letter
     for letter, occurrences in sorted_letters_by_value:
         print(f"The {letter} character was found {occurrences} times")
-
-
-# Functions
-def get_text(path):
-    with open(path) as f:
-        return f.read()
-    
-def get_total_words(text):
-    total_words = text.split()
-    return len(total_words)
-
-def count_letters(text):
-    letters_dict = {}
-    for letter in text:
-        if letter.isdigit() == False and letter.isalpha():
-            lowered_letter = letter.lower()
-            if lowered_letter not in letters_dict:
-                letters_dict[lowered_letter] = 1
-            else:
-                letters_dict[lowered_letter] += 1
-
-    return letters_dict
-
-def sort_on(item):
-    return item[1]
     
 main()
